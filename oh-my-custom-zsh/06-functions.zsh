cc57995f39c6e813309d510015fe11029eb3fa08
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2021-10-28 18:27:52 +0200

    new git config

--- oh-my-custom-zsh/06-functions.zsh
+++ oh-my-custom-zsh/06-functions.zsh
@@ -26,10 +26,10 @@ function server {
 function json() {
   if [ -p /dev/stdin ]; then
     # piping, e.g. `echo '{"foo":42}' | json`
-    python -mjson.tool | pygmentize -l javascript
+    python -mjson.tool
   else
     # e.g. `json '{"foo":42}'`
-    python -mjson.tool <<< "$*" | pygmentize -l javascript
+    python -mjson.tool <<< "$*"
   fi
 }
 
